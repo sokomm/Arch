5a910eac30ce85f57c2bbc06da68495298cd1530
sokomm <94488955+sokomm@users.noreply.github.com>
2021-11-16 21:06:05 +0000

    Add files via upload

--- 2-user.sh
+++ 2-user.sh
@@ -17,10 +17,24 @@ git clone "https://aur.archlinux.org/yay.git"
 cd ${HOME}/yay
 makepkg -si --noconfirm
 cd ~
+
+git clone "https://aur.archlinux.org/goodvibes.git"
+cd goodvibes
+makepkg -si --noconfirm
+cd ~
+
+git clone "https://aur.archlinux.org/viber.git"
+cd viber
+makepkg -si --noconfirm
+cd ~
+
 touch "$HOME/.cache/zshhistory"
 git clone "https://github.com/ChrisTitusTech/zsh"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
 ln -s "$HOME/zsh/.zshrc" $HOME/.zshrc
+
+
+
 
 PKGS=(
 'autojump'
@@ -29,9 +43,6 @@ PKGS=(
 'dxvk-bin' # DXVK DirectX to Vulcan
 'github-desktop-bin' # Github Desktop sync
 'lightly-git'
-'lightlyshaders-git'
-'mangohud' # Gaming FPS Counter
-'mangohud-common'
 'nerd-fonts-fira-code'
 'nordic-darker-standard-buttons-theme'
 'nordic-darker-theme'
@@ -47,8 +58,16 @@ PKGS=(
 'ttf-hack'
 'ttf-meslo' # Nerdfont package
 'ttf-roboto'
-'zoom' # video conferences
 'snap-pac'
+'remmina'
+'freerdp'
+'plank'
+'virtualbox'
+'wine'
+'openvpn'
+'network-manager-applet'
+'libreoffice-fresh'
+
 )
 
 for PKG in "${PKGS[@]}"; do
