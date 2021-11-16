815002c467b7704c483794c24ba5628e066381fd
sokomm <94488955+sokomm@users.noreply.github.com>
2021-11-16 22:09:54 +0100

    Update 3-post-setup.sh

--- 3-post-setup.sh
+++ 3-post-setup.sh
@@ -38,6 +38,8 @@ systemctl disable dhcpcd.service
 systemctl stop dhcpcd.service
 systemctl enable NetworkManager.service
 systemctl enable bluetooth
+systemctl start sshd
+systemctl enable sshd
 echo "
 ###############################################################################
 # Cleaning
