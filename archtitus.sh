4370ae972c32ba8996f36ff69795f8457c404cd8
sokomm <94488955+sokomm@users.noreply.github.com>
2022-01-28 21:20:15 +0100

    Update archtitus.sh

--- archtitus.sh
+++ archtitus.sh
@@ -5,4 +5,3 @@
     source /mnt/root/ArchTitus/install.conf
     arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchTitus/2-user.sh
     arch-chroot /mnt /root/ArchTitus/3-post-setup.sh
-    arch-chroot /mnt /root/ArchTitus/4-moje.sh
